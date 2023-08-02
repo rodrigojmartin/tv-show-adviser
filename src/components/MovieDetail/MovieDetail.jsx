80dfe449b92cea9da7008e8983de79e69a3b041e
Rodrigo Martin <rmartin@newrelic.com>
2023-08-02 10:10:53 +0200

    creates star rating component

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -1,9 +1,14 @@
+import { FiveStarRating } from "../FiveStarRating/FiveStarRating"
 import s from "./style.module.css"
 
 export function MovieDetail({movie}) {
+    const rating = movie.vote_average/2; 
     return <div>
         <div className={s.title}>{movie.name}</div>
-        <div className={s.rating}>{movie.vote_average/2}/5</div>
+        <div className={s.rating_container}>
+            <FiveStarRating rating={rating}/>
+            <span className={s.rating}>{rating}/5</span>
+        </div>
         <div className={s.overview}>{movie.overview}</div>
     </div>
 }
