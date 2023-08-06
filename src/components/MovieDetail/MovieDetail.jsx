540ac52f832b0e2adcb232bc87ff9190fbb62577
Rodrigo Martin <rmartin@newrelic.com>
2023-08-06 20:24:53 +0200

    bugfix: show movie title

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -4,7 +4,7 @@ import s from "./style.module.css"
 export function MovieDetail({movie}) {
     const rating = movie.vote_average/2; 
     return <div>
-        <div className={s.title}>{movie.name}</div>
+        <div className={s.title}>{movie.title}</div>
         <div className={s.rating_container}>
             <FiveStarRating rating={rating}/>
             <span className={s.rating}>{rating}/5</span>
