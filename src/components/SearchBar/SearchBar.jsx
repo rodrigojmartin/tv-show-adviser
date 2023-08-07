591e454385d5ad2869d73cf93ef1b21121e1540b
Rodrigo Martin <rmartin@newrelic.com>
2023-08-07 08:29:14 +0200

    cleans up search bar values after submit

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import s from "./style.module.css"
 import { Search as SearchIcon } from "react-bootstrap-icons"
 export function SearchBar({onSubmit}) {
 
+
+    const [value, setValue] = useState("");
+
     function submit(e) {
         if (e.key === "Enter" && e.target.value.trim != "") {
             onSubmit(e.target.value);
+            setValue("");
         }
             
+    }
+
+    function handleChange(e) {
+        setValue(e.target.value);   
     }
 
 
@@ -15,8 +24,10 @@ export function SearchBar({onSubmit}) {
         <SearchIcon size={27} className={s.icon}/>
         <input 
             onKeyUp={submit}
+            onChange={handleChange}
             className={s.input} 
             type="text" 
+            value={value}
             placeholder={"Search a movie you may like"}/>
     </>
     );
